6322e5ad83a916188b8c6f285f89b80f51050e24
India Cornell <indiaccornell@gmail.com>
2023-11-28 16:15:47 -0800

    Completed exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,13 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+# 1. Use Active Record's `create` class method multiple times to create 3 stores in the database:
+
+store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+# 2. Output (`puts`) the number of the stores using ActiveRecord's `count` method, to ensure that there are three stores in the database.
+
+puts Store.count
+
